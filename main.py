16195dea83d2c8a9b2c4fcbb549a497fcf4c6786
Adityakumar09 <kumarsinghaditya0902@gmail.com>
2024-11-24 19:15:55 +0530

    interpolated balls and only 2 players closest to court are shown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from utils import (read_video,save_video)
 from trackers import PlayerTracker,BallTracker
+from court_line_detector import CourtLineDetector
+import cv2
 
 
 def main():
@@ -21,9 +23,26 @@ def main():
                                                      stub_path="new/tracker_stubs/ball_detections.pkl"
                                                      )
     
+    ball_detections = ball_tracker.interpolate_ball_positions(ball_detections)
+
+    # Court Line Detector model
+    court_model_path = "new/models/keypoints_model.pth"
+    court_line_detector = CourtLineDetector(court_model_path)
+    court_keypoints = court_line_detector.predict(video_frames[0])
+
+    # choose players
+    player_detections = player_tracker.choose_and_filter_players(court_keypoints, player_detections)
+    
     ## Draw Player Bounding Boxes
     output_video_frames= player_tracker.draw_bboxes(video_frames, player_detections)
     output_video_frames= ball_tracker.draw_bboxes(output_video_frames, ball_detections)
+
+    ## Draw court Keypoints
+    output_video_frames  = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
+
+    ## Draw frame number on top left corner
+    for i, frame in enumerate(output_video_frames):
+        cv2.putText(frame, f"Frame: {i}",(10,30),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
 
     save_video(output_video_frames,"new/output_videos/output_video.avi")
